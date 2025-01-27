4d1ae418be525f65840ff971f0027891e98eeb48
bappie <55885320+p-Jimenez@users.noreply.github.com>
2025-01-27 18:58:42 +0100

    Update to Pixel v2

--- cpu.go
+++ cpu.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"image/color"
 	"io/ioutil"
-	"os"
+	"math/rand"
+	"strings"
 
-	"github.com/faiface/pixel"
-	"github.com/faiface/pixel/imdraw"
-	"github.com/faiface/pixel/pixelgl"
+	"github.com/gopxl/pixel/v2"
+	"github.com/gopxl/pixel/v2/backends/opengl"
+	"github.com/gopxl/pixel/v2/ext/imdraw"
+	"github.com/harry1453/go-common-file-dialog/cfd"
 	"golang.org/x/image/colornames"
 )
 
@@ -21,6 +25,7 @@ type CPU struct {
 	stack     [16]uint16
 	keyboard  Keyboard
 	superchip bool
+	rom       string
 }
 
 type Sprite struct {
@@ -29,7 +34,21 @@ type Sprite struct {
 }
 
 type Keyboard struct {
-	keys [16]uint8
+	keys [16]key
+}
+
+type key struct {
+	button pixel.Button
+	value  uint8
+}
+
+var keyboard = Keyboard{
+	keys: [16]key{
+		{pixel.Key1, 0x1}, {pixel.Key2, 0x2}, {pixel.Key3, 0x3}, {pixel.Key4, 0xC},
+		{pixel.KeyQ, 0x4}, {pixel.KeyW, 0x5}, {pixel.KeyE, 0x6}, {pixel.KeyR, 0xD},
+		{pixel.KeyA, 0x7}, {pixel.KeyS, 0x8}, {pixel.KeyD, 0x9}, {pixel.KeyF, 0xE},
+		{pixel.KeyZ, 0xA}, {pixel.KeyX, 0x0}, {pixel.KeyC, 0xB}, {pixel.KeyV, 0xF},
+	},
 }
 
 func (cpu *CPU) loadProgram(program []uint8) {
@@ -40,7 +59,7 @@ func (cpu *CPU) loadProgram(program []uint8) {
 
 const PIXEL_SIZE = 20.0
 
-func cpuLoop(cpu *CPU, window *pixelgl.Window) {
+func cpuLoop(cpu *CPU, window *opengl.Window) {
 
 	// Fallback to prevent index out of bounds
 	if (cpu.pc + 1) >= 4096 {
@@ -63,8 +82,7 @@ func cpuLoop(cpu *CPU, window *pixelgl.Window) {
 	}
 }
 
-func getProgram() []uint8 {
-	path := os.Args[1]
+func getProgram(path string) []uint8 {
 	dat, err := ioutil.ReadFile(path)
 
 	if err != nil {
@@ -81,7 +99,7 @@ func getProgram() []uint8 {
 }
 
 func initCpu() {
-	pixelgl.Run(runWindow)
+	opengl.Run(runWindow)
 }
 
 func fetch(cpu *CPU) uint16 {
@@ -90,10 +108,13 @@ func fetch(cpu *CPU) uint16 {
 	return opcode
 }
 
-func decode(cpu *CPU, opcode uint16, window *pixelgl.Window) {
+func decode(cpu *CPU, opcode uint16, window *opengl.Window) {
 	switch opcode & 0xF000 {
 	case 0x0000:
-		if opcode == 0x00EE {
+		switch opcode & 0x00FF {
+		case 0x00E0:
+			clearDisplay(cpu, window)
+		case 0x00EE:
 			returnFromSubroutine(cpu)
 		}
 	case 0x1000:
@@ -105,7 +126,7 @@ func decode(cpu *CPU, opcode uint16, window *pixelgl.Window) {
 	case 0x4000:
 		skipIfNotEqual(cpu, opcode)
 	case 0x5000:
-		// TODO: skipIfEqualRegisters(cpu, opcode)
+		skipIfEqualRegisters(cpu, opcode)
 	case 0x6000:
 		setRegister(cpu, opcode)
 	case 0x7000:
@@ -136,32 +157,32 @@ func decode(cpu *CPU, opcode uint16, window *pixelgl.Window) {
 	case 0xA000:
 		setIndexRegister(cpu, opcode)
 	case 0xB000:
-		// TODO: jumpToAddressPlusRegister(cpu, opcode)
+		jumpToAddressPlusRegister(cpu, opcode)
 	case 0xC000:
-		// TODO: setRegisterToRandom(cpu, opcode)
+		setRegisterToRandom(cpu, opcode)
 	case 0xD000:
 		drawSprite(cpu, opcode, window)
 	case 0xE000:
 		switch opcode & 0x00FF {
 		case 0x009E:
-			// TODO: skipIfKeyPressed(cpu, opcode)
+			skipIfKeyPressed(cpu, opcode, window)
 		case 0x00A1:
-			// TODO: skipIfKeyNotPressed(cpu, opcode)
+			skipIfKeyNotPressed(cpu, opcode, window)
 		}
 	case 0xF000:
 		switch opcode & 0x00FF {
 		case 0x0007:
-			// TODO: setRegisterToDelayTimer(cpu, opcode)
+			setRegisterToDelayTimer(cpu, opcode)
 		case 0x000A:
-			// TODO: waitForKeyPress(cpu, opcode)
+			waitForKeyPress(cpu, opcode, window)
 		case 0x0015:
-			// TODO: setDelayTimer(cpu, opcode)
+			setDelayTimer(cpu, opcode)
 		case 0x0018:
-			// TODO: setSoundTimer(cpu, opcode)
+			setSoundTimer(cpu, opcode)
 		case 0x001E:
 			addToIndexRegister(cpu, opcode)
 		case 0x0029:
-			// TODO: setIndexRegisterToSprite(cpu, opcode)
+			setIndexRegisterToSprite(cpu, opcode)
 		case 0x0033:
 			storeBinaryCodedDecimal(cpu, opcode)
 		case 0x0055:
@@ -172,9 +193,10 @@ func decode(cpu *CPU, opcode uint16, window *pixelgl.Window) {
 	}
 }
 
-func clearDisplay(cpu *CPU, window *pixelgl.Window) {
+func clearDisplay(cpu *CPU, window *opengl.Window) {
+	fmt.Println("Clearing display")
 	imd := imdraw.New(nil)
-	imd.Color = colornames.Black
+	imd.Color = color.RGBA{10, 10, 10, 255}
 
 	imd.Push(pixel.V(0, 0), pixel.V(64*PIXEL_SIZE, 32*PIXEL_SIZE))
 	imd.Rectangle(0)
@@ -288,7 +310,15 @@ func setIndexRegister(cpu *CPU, opcode uint16) {
 	cpu.i = opcode & 0x0FFF
 }
 
-func drawSprite(cpu *CPU, opcode uint16, window *pixelgl.Window) {
+func jumpToAddressPlusRegister(cpu *CPU, opcode uint16) {
+	cpu.pc = (opcode & 0x0FFF) + uint16(cpu.v[0])
+}
+
+func setRegisterToRandom(cpu *CPU, opcode uint16) {
+	cpu.v[(opcode&0x0F00)>>8] = uint8((opcode & 0x00FF) & uint16(rand.Intn(255)))
+}
+
+func drawSprite(cpu *CPU, opcode uint16, window *opengl.Window) {
 	vx := cpu.v[(opcode&0x0F00)>>8]
 	vy := cpu.v[(opcode&0x00F0)>>4]
 	nibble := opcode & 0x000F
@@ -312,6 +342,22 @@ func drawSprite(cpu *CPU, opcode uint16, window *pixelgl.Window) {
 				imd.Push(rect.Min, rect.Max)
 				imd.Rectangle(0)
 				imd.Draw(window)
+			} else {
+				// draw black rectangle
+
+				rect := pixel.R(
+					float64(vx+j)*PIXEL_SIZE,
+					float64(window.Bounds().Max.Y)-float64(vy+i+1)*PIXEL_SIZE, // Adjusted Y-coordinate
+					float64(vx+j+1)*PIXEL_SIZE,
+					float64(window.Bounds().Max.Y)-float64(vy+i)*PIXEL_SIZE, // Adjusted Y-coordinate
+				)
+
+				imd := imdraw.New(nil)
+				imd.Color = color.RGBA{10, 10, 10, 255}
+
+				imd.Push(rect.Min, rect.Max)
+				imd.Rectangle(0)
+				imd.Draw(window)
 			}
 		}
 	}
@@ -328,8 +374,10 @@ func callSubroutine(cpu *CPU, address uint16) {
 }
 
 func returnFromSubroutine(cpu *CPU) {
-	cpu.sp--
-	cpu.pc = cpu.stack[cpu.sp]
+	if cpu.sp > 0 {
+		cpu.sp--
+		cpu.pc = cpu.stack[cpu.sp]
+	}
 }
 
 func skipIfNotEqual(cpu *CPU, opcode uint16) {
@@ -344,26 +392,151 @@ func skipIfNotEqualRegisters(cpu *CPU, opcode uint16) {
 	}
 }
 
+func skipIfEqualRegisters(cpu *CPU, opcode uint16) {
+	if cpu.v[(opcode&0x0F00)>>8] == cpu.v[(opcode&0x00F0)>>4] {
+		cpu.pc += 2
+	}
+}
+
+func skipIfKeyPressed(cpu *CPU, opcode uint16, window *opengl.Window) {
+	vx := cpu.v[(opcode&0x0F00)>>8]
+
+	fmt.Printf("OpCode: %X\n", opcode)
+	fmt.Printf("Checking if key %X is not pressed\n", vx)
+
+	if window.Pressed(findKey(cpu.keyboard, vx)) {
+		cpu.pc += 2
+	}
+}
+
+func skipIfKeyNotPressed(cpu *CPU, opcode uint16, window *opengl.Window) {
+	vx := cpu.v[(opcode&0x0F00)>>8]
+
+	fmt.Printf("OpCode: %X\n", opcode)
+	fmt.Printf("Checking if key %X is pressed\n", vx)
+
+	if !window.Pressed(findKey(cpu.keyboard, vx)) {
+		cpu.pc += 2
+	}
+}
+
+func waitForKeyPress(cpu *CPU, opcode uint16, window *opengl.Window) {
+	vx := cpu.v[(opcode&0x0F00)>>8]
+
+	fmt.Printf("OpCode: %X\n", opcode)
+	fmt.Printf("Waiting for key press %X\n", vx)
+
+	key := findKey(cpu.keyboard, vx)
+
+	if !window.Pressed(key) {
+		cpu.pc -= 2
+	}
+
+}
+
+func findKey(keyboard Keyboard, value uint8) pixel.Button {
+	fmt.Printf("Looking for key: %X\n", value)
+	for i := 0; i < len(keyboard.keys); i++ {
+		if keyboard.keys[i].value == value {
+			return keyboard.keys[i].button
+		}
+	}
+
+	panic("Key not found")
+}
+
+func setRegisterToDelayTimer(cpu *CPU, opcode uint16) {
+	cpu.v[(opcode&0x0F00)>>8] = cpu.dt
+}
+
+func setDelayTimer(cpu *CPU, opcode uint16) {
+	cpu.dt = cpu.v[(opcode&0x0F00)>>8]
+}
+
+func setSoundTimer(cpu *CPU, opcode uint16) {
+	cpu.st = cpu.v[(opcode&0x0F00)>>8]
+}
+
+func setIndexRegisterToSprite(cpu *CPU, opcode uint16) {
+	cpu.i = uint16(cpu.v[(opcode&0x0F00)>>8]) * 5
+}
+
 func runWindow() {
 
-	program := getProgram()
+	cpu := CPU{
+		keyboard: keyboard,
+	}
 
-	cfg := pixelgl.WindowConfig{
+	cfg := opengl.WindowConfig{
 		Title:  "Chip-8 Emulator",
 		Bounds: pixel.R(0, 0, 64*PIXEL_SIZE, 32*PIXEL_SIZE),
 	}
 
-	win, err := pixelgl.NewWindow(cfg)
+	win, err := opengl.NewWindow(cfg)
 	if err != nil {
 		panic(err)
 	}
 
-	cpu := CPU{}
-
-	cpu.loadProgram(program)
+	openProgram(win, &cpu)
 
 	for !win.Closed() {
+
+		switch {
+		case win.JustPressed(pixel.KeyEscape):
+			win.SetClosed(true)
+
+		case win.JustPressed(pixel.KeyF1):
+			openProgram(win, &cpu)
+			// case win.JustPressed(pixel.KeyF2):
+			// 	saveState(&cpu)
+			// case win.JustPressed(pixel.KeyF3):
+			// 	loadState(&cpu, win)
+		}
+
 		cpuLoop(&cpu, win)
 		win.Update()
 	}
+
+}
+
+func openProgram(win *opengl.Window, cpu *CPU) {
+
+	// ask for load file
+	fmt.Println("Opening file picker")
+
+	dialog, error := cfd.NewOpenFileDialog(cfd.DialogConfig{
+		Title: "Load Chip-8 ROM",
+		FileFilters: []cfd.FileFilter{
+			{
+				DisplayName: "Chip-8 ROM",
+				Pattern:     "*.ch8",
+			},
+		},
+	})
+
+	if error != nil {
+		panic(error)
+	}
+
+	path, err := dialog.ShowAndGetResult()
+
+	rom := strings.ReplaceAll(strings.ReplaceAll(path, "\\", "/"), ".ch8", "")
+
+	if err != nil {
+		panic(err)
+	}
+
+	program := getProgram(path)
+
+	cpu.v = [16]uint8{}
+	cpu.i = 0
+	cpu.pc = 0x200
+	cpu.sp = 0
+	cpu.dt = 0
+	cpu.st = 0
+	cpu.stack = [16]uint16{}
+	cpu.keyboard = keyboard
+	cpu.rom = rom[strings.LastIndex(rom, "/")+1:]
+
+	cpu.loadProgram(program)
 }
--- go.mod
+++ go.mod
@@ -1,14 +1,22 @@
 module chip-8
 
-go 1.18
+go 1.21
+
+toolchain go1.22.6
 
 require (
-	github.com/faiface/glhf v0.0.0-20181018222622-82a6317ac380 // indirect
-	github.com/faiface/mainthread v0.0.0-20171120011319-8b78f0a41ae3 // indirect
-	github.com/faiface/pixel v0.10.0 // indirect
-	github.com/go-gl/gl v0.0.0-20190320180904-bf2b1f2f34d7 // indirect
-	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20191125211704-12ad95a8df72 // indirect
-	github.com/go-gl/mathgl v0.0.0-20190416160123-c4601bc793c7 // indirect
-	github.com/pkg/errors v0.8.1 // indirect
-	golang.org/x/image v0.0.0-20190523035834-f03afa92d3ff // indirect
+	github.com/gopxl/pixel/v2 v2.3.0
+	github.com/harry1453/go-common-file-dialog v1.2.0
+	golang.org/x/image v0.23.0
+)
+
+require (
+	github.com/go-gl/gl v0.0.0-20231021071112-07e5d0ea2e71 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20240506104042-037f3cc74f2a // indirect
+	github.com/go-gl/mathgl v1.1.0 // indirect
+	github.com/go-ole/go-ole v1.2.4 // indirect
+	github.com/google/uuid v1.1.1 // indirect
+	github.com/gopxl/glhf/v2 v2.0.0 // indirect
+	github.com/gopxl/mainthread/v2 v2.1.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 )
